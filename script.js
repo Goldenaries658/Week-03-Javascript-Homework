a2f3d6bd1a76bcb3a61116c95265aa3562c16679
Goldenaries <jacob.houston658@gmail.com>
2020-04-02 21:15:07 +0100

    Created prompt for pw length with error handler

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
+
+var passwordLength = 0;
+
+function generatePassword() {
+  var passwordLength = prompt(
+    'Choose a password length. (Must be 8-128 characters)'
+  );
+  while (passwordLength > 128 || passwordLength < 8) {
+    if (passwordLength === null) {
+      close();
+    } else {
+      alert('Password has to be between 8-128 characters.');
+      passwordLength = prompt('Choose a password length.');
+    }
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
