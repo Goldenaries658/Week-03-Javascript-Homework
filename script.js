bc9bf274e10bd756d350655630062a46b0ae2771
Goldenaries <jacob.houston658@gmail.com>
2020-04-03 23:23:31 +0100

    Changed charSet to CharType & added error handling

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 
-
 function generatePassword() {
   var passwordLen = prompt(
     'Choose a password length.\n(Must be 8-128 characters)'
@@ -19,37 +18,60 @@ function generatePassword() {
     }
     alert('Password has to be between 8-128 characters.');
     passwordLen = prompt('Choose a password length.');
+
     if (passwordLen === null) {
       break;
     }
   }
 
-  var charSet = {};
+  var charType = {};
   var charProps = [
     'Uppercase characters',
     'Lowercase characters',
     'Numbers',
     'Special characters'
   ];
+  var charTypeCount = 0;
 
-  for (i = 0; i < 4; i++) {
-    var chars = charSet[charProps[i]];
-    chars = prompt(charProps[i] + '? (Y/N)');
+  while (charTypeCount == 0) {
+    for (i = 0; i < 4; i++) {
+      var chars = charType[charProps[i]];
+      chars = prompt(charProps[i] + '? (Y/N)');
+
+      if (chars === null) {
+        break;
+      }
+
+      chars = chars.toLowerCase();
+
+      while (chars != 'y' && chars != 'n') {
+        alert('Enter Y or N.');
+        chars = prompt(charProps[i] + '?');
+
+        if (chars === null) {
+          break;
+        }
+      }
+
+      if (chars == 'y') {
+        charTypeCount++;
+      }
+    }
 
     if (chars === null) {
       break;
     }
 
-    chars = chars.toLowerCase();
-
-    while (chars != 'y' && chars != 'n') {
-      alert('Enter Y or N.');
-      chars = prompt(charProps[i] + '?');
-      if (chars === null) {
-        break;
-      }
+    if (charTypeCount == 0) {
+      alert('You need to select at least one character type.');
     }
   }
+
+  if (chars === null) {
+    return 'Cancelled';
+  }
+
+  
 }
 // Write password to the #password input
 function writePassword() {
