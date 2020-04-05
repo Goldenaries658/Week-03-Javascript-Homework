b91c0b83d30490e94a7b23715e0a872c51e6a03f
Goldenaries <jacob.houston658@gmail.com>
2020-04-05 15:30:44 +0100

    Added character set generators and arrays

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 
+//Character sets
+
+//Generates a lowercase alphabet
+function genLowCase() {
+  var lowCase = [];
+  for (i = 10, a = 0; i < 36; i++, a++) {
+    lowCase[a] = i.toString(36);
+  }
+  return lowCase;
+}
+
+//Generates an uppercase alphabet
+function genUpCase() {
+  var upCase = [];
+  for (i = 10, a = 0; i < 36; i++, a++) {
+    letter = i.toString(36);
+    upCase[a] = letter.toUpperCase();
+  }
+  return upCase;
+}
+
+var numsSet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+//Creates an array of special chars, this is to save space in code
+specChars = '! # $ % & " ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _ ` { | } ~ \'';
+var specSet = specChars.split(' ');
+
 function generatePassword() {
   var passwordLen = prompt(
     'Choose a password length.\n(Must be 8-128 characters)'
@@ -12,7 +39,6 @@ function generatePassword() {
 
   while (passwordLen < 8 || passwordLen > 128 || isNaN(passwordLen)) {
     passwordLen = parseInt(passwordLen, 10);
-    console.log(passwordLen);
     if (isNaN(passwordLen)) {
       alert('Enter a number.');
     }
@@ -24,19 +50,18 @@ function generatePassword() {
     }
   }
 
-  var charType = {};
+  var charList = [];
   var charProps = [
-    'Uppercase characters',
     'Lowercase characters',
+    'Uppercase characters',
     'Numbers',
-    'Special characters'
+    'Special characters',
   ];
   var charTypeCount = 0;
 
   while (charTypeCount == 0) {
     for (i = 0; i < 4; i++) {
-      var chars = charType[charProps[i]];
-      chars = prompt(charProps[i] + '? (Y/N)');
+      var chars = prompt(charProps[i] + '? (Y/N)');
 
       if (!chars) {
         break;
@@ -70,8 +95,6 @@ function generatePassword() {
   if (!chars) {
     return 'Cancelled';
   }
-
-
 }
 // Write password to the #password input
 function writePassword() {
